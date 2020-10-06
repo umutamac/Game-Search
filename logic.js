8e8f05b5b0eb4ac2c4d36b19245c3dcb8a370c7e
Umut Alptekin <amacalptekin@gmail.com>
2020-10-06 20:01:03 +0300

    m

--- logic.js
+++ logic.js
@@ -6,8 +6,10 @@ $("#searchReviewsButton").on("click", function(event){
 
 $("#searchDealsButton").on("click", function (event) {
     event.preventDefault();
-    saveSearch(searchTerm);
-
+    saveSearch( $("#userGame").val() ); // Store the user input
+    if($("#userGame").val() == ""){ // if search input is empty, dont do anything
+        return;
+    }
     //var queryURL = "https://www.cheapshark.com/api/1.0/games?title=" + searchTerm + "&limit=5&exact=0";
     var queryURL2 = "https://www.cheapshark.com/api/1.0/deals?&title=" + searchTerm + "&pageSize=5"
     $.ajax({
@@ -18,7 +20,7 @@ $("#searchDealsButton").on("click", function (event) {
         $("#userGame").val("");
         $("#deals").append(response2[0].title);
     })
-})
+});
 
 
 //--------------------------------- Local Storage
