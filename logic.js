3a8bf1d45bfe0030379adbdc983227a48f88795e
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-06 11:27:02 -0500

    added other button, amended original on click function and queryurl2

--- logic.js
+++ logic.js
@@ -1,12 +1,18 @@
-$("#searchButton").on("click", function (event) {
+$("#searchReviewsButton").on("click", function(event){
+    event.preventDefault();
+});
+
+$("#searchDealsButton").on("click", function (event) {
     event.preventDefault();
     var searchTerm = $("#userGame").val();
-    var queryURL = "https://www.cheapshark.com/api/1.0/games?title=" + searchTerm + "&limit=5&exact=0";
+    var queryURL2 = "https://www.cheapshark.com/api/1.0/deals?&title=" + searchTerm + "&pageSize=5"
+    // "https://www.cheapshark.com/api/1.0/games?title=" + searchTerm + "&limit=5&exact=0";
     $.ajax({
-        url: queryURL,
+        url: queryURL2,
         method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        // $("#userGame").text("");
+    }).then(function (response2) {
+        console.log(response2);
+        $("#userGame").val("");
+        $("#deals").append(response2[0].title);
     })
 })
